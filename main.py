7b17c90642e312c56a27e31ca980bb1d70cbb66d
Sudipra Biswas <sudipra.biswas@cytel.com>
2024-08-09 02:07:34 +0530

    Implementing scheams in post request to validate the payload data

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 
+from validator import Post
+
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return({"message": "Welcome to home page of fast api!"})
+    return({"message": "Welcome to the home page of Fast API!"})
+
+@app.get("/posts")
+def show_posts():
+    return({"message": "Show all posts"})
+
+@app.post("/posts")
+def create_post(post: Post):
+    print(post.rating)
+    print(post.model_dump()) # use to convert the payload to dictionary/JSON.
+    return({"message": "new post"})
